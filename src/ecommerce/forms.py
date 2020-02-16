da1a3b10181e00200f52f79214f592663b403573
shani rabi guttel <rabbi.shani@gmail.com>
2020-02-16 05:51:22 +0200

    login/register design pages

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -60,14 +60,74 @@ class ContactForm(forms.Form):
         return email
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+                widget=forms.TextInput(
+                        attrs={
+                            "class": "form-control form-control-lg col-md-12 form-group rtl",
+                            # "placeholder": "שם משתמש"
+                            }
+                        ),
+                    label='שם משתמש'
+                )
+
+    password = forms.CharField(
+                    widget=forms.PasswordInput(
+                            attrs={
+                                "class": "form-control form-control-lg col-md-12 form-group rtl",
+                                # "placeholder": "סיסמה"
+                                }
+                            ),
+                        label='סיסמה'
+                    )
+    # username = forms.CharField()
+    # password = forms.CharField(widget=forms.PasswordInput)
 
 class RegisterForm(forms.Form):
-    username = forms.CharField()
-    email    = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+    username = forms.CharField(
+                widget=forms.TextInput(
+                        attrs={
+                            "class": "form-control form-control-lg col-md-12 form-group rtl",
+                            # "placeholder": "שם משתמש"
+                            }
+                        ),
+                    label='שם משתמש'
+                )
+
+    email = forms.EmailField(
+            widget=forms.EmailInput(
+                    attrs={
+                        "class": "form-control form-control-lg col-md-12 form-group",
+                        # "placeholder": "כתובת מייל"
+                        }
+                    ),
+                label='כתובת מייל'
+            )
+
+    password = forms.CharField(
+                    widget=forms.PasswordInput(
+                            attrs={
+                                "class": "form-control form-control-lg col-md-12 form-group rtl",
+                                # "placeholder": "סיסמה"
+                                }
+                            ),
+                        label='סיסמה'
+                    )
+    password2 = forms.CharField(
+                    widget=forms.PasswordInput(
+                            attrs={
+                                "class": "form-control form-control-lg col-md-12 form-group rtl",
+                                # "placeholder": "אימות סיסמה"
+                                }
+                            ),
+                        label='אימות סיסמה'
+                    )
+
+
+    #
+    # username = forms.CharField()
+    # email    = forms.EmailField()
+    # password = forms.CharField(widget=forms.PasswordInput)
+    # password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
