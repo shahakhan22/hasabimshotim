2bef4ea8b9098196a34a77d0346c916f8cbbe1bc
shani rabi guttel <rabbi.shani@gmail.com>
2020-02-19 05:30:44 +0200

    add requirements.txt file

--- src/products/admin.py
+++ src/products/admin.py
@@ -4,7 +4,7 @@ from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'slug', 'price']
+    list_display = ['__str__', 'slug', 'price', 'active']
     class Meta:
         model = Product
 
